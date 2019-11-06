e0f963b8982f046966413db55f76d3864f22eab4
Mathieu <mathieu.gery@epitech.eu>
2019-11-06 22:09:21 +0100

    [+] topbar for dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import API from "../../utils/API";
 import SideBar from "./SideBar";
+import MyCard from "./MyCard";
 
 export class Dashboard extends React.Component {
     disconnect = () => {
@@ -9,7 +10,13 @@ export class Dashboard extends React.Component {
         window.location = "/";
     };
     render() {
-        return (<SideBar/>
+        return (
+            <div>
+                <SideBar/>
+                <div className={"h-screen bg-indigo-100 px-8 py-8"}>
+                    <MyCard/>
+                </div>
+            </div>
         );
     }
 }
--- client/src/components/Dashboard/SideBar.js
+++ client/src/components/Dashboard/SideBar.js
@@ -1,26 +1,45 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'
 
 class SideBar extends React.Component {
     render() {
         return (
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-5xl mb-2">This is a title</div>
-                        <p className="text-gray-700 text-base text-3xl">
-                            fzaoifzajf jzafizafoijzaifozajfeoia jzifjoizafijdsiqjf oiqjifej oijsoiqjifjqljes ifjiajzeoi fj
-                        </p>
-                    </div>
-                <div className="px-6 py-4">
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+            <nav className="flex items-center justify-between flex-wrap bg-indigo-900 p-6">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <FontAwesomeIcon icon={faChartLine} className={"text-6xl"}/>
+                    <span className="font-semibold text-3xl tracking-tight p-4">Dashboard</span>
                 </div>
-            </div>
+                <div className="block lg:hidden">
+                    <button
+                        className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>Menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                        </svg>
+                    </button>
+                </div>
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <div className="text-sm lg:flex-grow">
+                        <a href="#responsive-header"
+                           className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Docs
+                        </a>
+                        <a href="#responsive-header"
+                           className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Examples
+                        </a>
+                        <a href="#responsive-header"
+                           className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                            Blog
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#"
+                           className="inline-block text-3xl px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Out</a>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
