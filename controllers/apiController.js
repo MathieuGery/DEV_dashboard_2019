90ebc3349a8b3a9704ff53bda9de3695e7e3b757
Mathieu <mathieu.gery@epitech.eu>
2019-11-13 11:40:47 +0100

    [+] open weather implementaion api side

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,7 +1,8 @@
 const about = require('./about/about');
-const api = require('./services/rss/rss');
+const api = require('./services/api');
 
 module.exports = function (app) {
     app.get('/about.json', about.about);
     app.get('/rss', api.rss);
+    app.post('/weather', api.weather);
 };
---
+++ controllers/services/api.js
@@ -0,0 +1,39 @@
+var Feed = require('rss-to-json');
+const superagent = require('superagent');
+
+
+const burl = "api.openweathermap.org/data/2.5/weather";
+const appid_weather = "5395bcad51f4b37afe5b814977728a96";
+
+async function rss(req, res) {
+    let result = await Feed.load('https://learnstartup.net/feed/');
+    try {
+        return res.status(200).json(
+            {result}
+        );
+    } catch (error) {
+        return res.status(500).json({
+            error
+        });
+    }
+}
+
+async function weather(req, res) {
+    const {q} = req.query;
+    console.log(q, appid_weather);
+    if (!q || !appid_weather) {
+        return res.status(400).json({
+            text: "Invalid request city can not be null!"
+        });
+    }
+    superagent.get(`${burl}`)
+        .query({ q: q, appid: appid_weather })
+        .end((err, resp) => {
+            if (err) { return res.status(200).json(resp.body); }
+            console.log(resp.body);
+            return res.status(200).json(resp.body);
+           });
+}
+
+exports.weather = weather;
+exports.rss = rss;
